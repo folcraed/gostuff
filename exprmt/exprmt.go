260737a6489ef74615ca65b6686227168b59713a
folcred <robttheb@gmail.com>
2024-02-14 12:50:18 -0700

    Experiment with creating modules and logging
    I thought this worth saving, as it's an example of creating and
    using modules, as well as creating a logging structure for things
    like errors, debugging, etc.

--- exprmt/exprmt.go
+++ exprmt/exprmt.go
@@ -2,6 +2,14 @@
 
 package main
 
-func main() {
+import (
+	"rwb/exprmt/logging"
+	"time"
+)
 
+func main() {
+	logger := logging.New(time.DateTime, false)
+	logger.Log("info", "starting up service")
+	logger.Log("warning", "no tasks found")
+	logger.Log("error", "exiting: no work performed")
 }
