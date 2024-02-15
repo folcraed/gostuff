8b05919435af4e07d2a0325968df0f0d34e7b0d2
folcred <robttheb@gmail.com>
2024-02-15 06:54:07 -0700

    For use as a template, a starting point

--- exprmt/exprmt.go
+++ exprmt/exprmt.go
@@ -3,13 +3,9 @@
 package main
 
 import (
-	"rwb/exprmt/logging"
-	"time"
+	"fmt"
 )
 
 func main() {
-	logger := logging.New(time.DateTime, false)
-	logger.Log("info", "starting up service")
-	logger.Log("warning", "no tasks found")
-	logger.Log("error", "exiting: no work performed")
+	fmt.Println("Hi there!")
 }
