c3f8802de49ce434ba331ca09567b933a55eb155
folcred <robttheb@gmail.com>
2024-03-26 20:51:48 -0600

    Changed user input code
    Changed the multiple prompts for the 2nd date to use one input.

--- datecalc/datecalc.go
+++ datecalc/datecalc.go
@@ -8,25 +8,22 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"strings"
 	"os"
+	"strings"
+	"time"
 )
 
 // User prompts with instructions
 var message = "Enter the first date as '2023-06-12' (Year-Month-Day).\nNote single-digit months and days MUST have '0' prefix."
 var getDiff = `Now enter the time difference, '23 8 6' to calculate
 into the future, '-22 -5 -10' to calculate into the past.
-Each will be asked for individually.`
-
-// Variables to hold the date differences
-var getYear = "\nEnter the year value: "
-var getMonth = "Enter the month value: "
-var getDay = "Enter the day value: "
+You can also enter full dates like '1959 10 5' to get how
+many years, months and days are between them.`
 
 func main() {
 	var timeInput string
 	fmt.Println(message)
+	fmt.Println()
 	fmt.Scanf("%s", &timeInput) // Get the starting date from the user
 	theFirst, err := time.Parse(time.DateOnly, timeInput)
 	if err != nil {
@@ -39,16 +36,11 @@ func main() {
 	var month int
 	var day int
 
-	// Routines to get the date difference and store them
+	// Get the date difference and store it
 	fmt.Println()
 	fmt.Println(getDiff)
 	fmt.Println()
-	fmt.Print(getYear)
-	fmt.Scanf("%d", &year)
-	fmt.Print(getMonth)
-	fmt.Scanf("%d", &month)
-	fmt.Print(getDay)
-	fmt.Scanf("%d", &day)
+	fmt.Scan(&year, &month, &day)
 
 	// Take the difference and add or subtract it from the original date
 	difference := theFirst.AddDate(year, month, day)
